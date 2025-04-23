9428fa1643ab5ef4686a148f9c892cfbcae85409
goto-del <eliasmoncy27@gmail.com>
2025-04-23 15:19:50 +0530

    Add files via upload

---
+++ Turtle 3.0.py
@@ -0,0 +1,11 @@
+import turtle
+my_wn = turtle.Screen()
+my_wn.bgcolor("light blue")
+my_wn.title("Turtle")
+my_pen = turtle.Turtle()
+size = 0
+while True:
+    my_pen.fd(size+1)
+    my_pen.left(90)
+    size = size - 5
+size = size+1
