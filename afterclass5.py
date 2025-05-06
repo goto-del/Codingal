6fbe793c54891de479a08b13de4a058b836ee675
goto-del <eliasmoncy27@gmail.com>
2025-05-06 16:44:57 +0530

    Add files via upload

---
+++ afterclass5.py
@@ -0,0 +1,7 @@
+Elias_friends = ["Eric" , "Abel" , "Shaurya" , "Advait" , "Harshul"]
+Eric = "Eric: 24 December"
+Abel = "Abel: 5 October"
+Shaurya = "Shaurya: 2 November"
+Advait = "Advait: 12 January"
+Harshul = "Harshul: 5 June"
+print(Eric,Abel,Shaurya,Advait,Harshul)
---
+++ afterclass6.py
@@ -0,0 +1,3 @@
+number = int(input("Enter a number: "))
+square_root = int(number ** 0.5)
+print("Square root is:", square_root)
