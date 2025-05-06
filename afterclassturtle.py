6781b2cb7313a007273b5c290a9105e927a107df
goto-del <eliasmoncy27@gmail.com>
2025-05-06 16:29:52 +0530

    Add files via upload

---
+++ afterclassturtle.py
@@ -0,0 +1,22 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("light blue")
+
+pen = turtle.Turtle()
+pen.pensize(3)
+pen.color("dark blue")
+
+pen.forward(100)
+pen.right(90)
+
+pen.forward(100)
+pen.right(90)
+
+pen.forward(100)
+pen.right(90)
+
+pen.forward(100)
+pen.right(90)
+
+turtle.done()
