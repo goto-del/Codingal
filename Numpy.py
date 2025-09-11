a8407f417a2767b2db9afdb158ea52f106a30cdc
goto-del <eliasmoncy27@gmail.com>
2025-09-11 15:43:07 +0530

    Add files via upload

---
+++ Numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.array([1, 3, 4, 6, 9, 2, 8, 5, 7, 10, 13, 15, 17, 21, 16, 19, 14, 11, 12, 18, 20])
+print(a)
+
+print(type(a))
+print(a[3])
+print(a[0:5])
+print(a.dtype)
+print(a.shape)
+print(np.sort(a))
