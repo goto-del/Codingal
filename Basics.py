64a8ba0047a97e347b198b870b5557eb5e92e2f9
goto-del <eliasmoncy27@gmail.com>
2025-10-15 15:08:25 +0530

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,16 @@
+print("Hello, World!")
+a = 10
+print(a)
+
+sum = 10
+print("The value of sum is:", sum)
+print(f"The value of sum is: {sum}")
+
+ab = 5
+print(ab * 10)
+
+print("Hello, World\nWelcome to Python Programming Language")
+
+import keyword
+print(keyword.kwlist)
+
