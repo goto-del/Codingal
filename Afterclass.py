04d2c21ebe671df1a6bacd371dfa29e47df61221
goto-del <eliasmoncy27@gmail.com>
2025-11-06 15:22:07 +0530

    Add files via upload

---
+++ Afterclass.py
@@ -0,0 +1,9 @@
+s1 = {1, 2, 3, 4, 3, 2, 5, 1}
+Names = {"Alice", "Bob", "Charlie", "Alice", "Eve", "Alice", "Eve", "Bob"}
+print(s1)
+print(Names)
+
+# To convert set to list
+list_from_set = list(s1)
+print(list_from_set)
+quit
