cb126fe5ee30bb78a36ea3a926a3482b7809a3fa
goto-del <eliasmoncy27@gmail.com>
2025-11-20 15:27:35 +0530

    Add files via upload

--- Afterclass.py
+++ Afterclass.py
@@ -1,31 +1,38 @@
-class Polygon:
-    def area(self):
-        pass
-
-class Rectangle(Polygon):
-    def __init__(self, length, breadth):
-        self.length = length
-        self.breadth = breadth
-    def area(self):
-        return self.length * self.breadth
-
-class Square(Polygon):
-    def __init__(self, side):
-        self.side = side
+#HELP me
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
     def area(self):
-        return self.side * self.side
+        return self.width * self.height
     
-class Circle(Polygon):
+class Square:
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    def area(self):
+        return self.side_length ** 2
+class Circle:
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        return self.radius * self.radius * 3.1416
-    
-r1 = Rectangle(22.35 , 3)
-print(r1.area())
-s1 = Square(34)
-print(s1.area())
-c1 = Circle(35)
-print(c1.area())
+        import math
+        return math.pi * (self.radius ** 2)
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
+rec1 = Rectangle(10, 5)
+print("Area of Rectangle:", rec1.area())
+sq1 = Square(4)
+print("Area of Square:", sq1.area())
+cir1 = Circle(3)
+print("Area of Circle:", cir1.area())
+tri1 = Triangle(6, 8)
+print("Area of Triangle:", tri1.area())
