6fd315884ba1833b7b043c3ded1f969e0d7add81
goto-del <eliasmoncy27@gmail.com>
2025-12-18 15:17:56 +0530

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,35 +1,8 @@
-import tkinter as tk
-
-def cal():
-    amount = int(e1.get())
-    note2000 = amount // 2000
-    amount = amount%2000
-    note500 = amount // 500
-    amount =  amount%500
-    note100 = amount // 100
-    amount = amount%100
-    note1 = amount // 1
+def sum(n):
+    return n * (n+1) / 2
 
 
-    result.config(text=f"₹2,000 notes : {note2000}\n ₹500 notes : {note500}\n ₹100 notes : {note100}\n ₹1 notes : {note1}\n")
-    
-    
+n = int(input("Enter a number : "))
 
-root = tk.Tk()
-root.title("Denomination Calculator")
-root.geometry("300x300")
-
-
-l1 = tk.Label(root ,text="Enter the Amount in Rupees", font = ("Arial", 13))
-l1.pack(pady=10)
-
-e1 = tk.Entry(root, width=200)
-e1.pack(pady=10)
-
-b1 = tk.Button(root, text="Calculate", fg = "white", bg="black", command=cal)
-b1.pack(pady=10)
-
-result = tk.Label(root, text="", font=("Arial", 15))
-result.pack(pady=10)
-
-root.mainloop()
+s = sum(n)
+print(s)
--- Main3.py
+++ Main3.py
@@ -1,12 +1,11 @@
 def sum(n):
     sum = 0
     for i in range(1, n+1):
-        sum = sum+i
+        for j in range(1, i+1):
+            sum = sum+1
 
     return sum
 
 n = int(input("Enter a number : "))
-
 s = sum(n)
-
 print(s)
