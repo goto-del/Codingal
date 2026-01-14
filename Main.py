e1f076462b2e7746184c87011e9e4a0407c82d6c
goto-del <eliasmoncy27@gmail.com>
2026-01-14 15:31:28 +0530

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,8 +1,15 @@
-def printes(n):   
-    if(n<=0):
-        return
-    print("Codingal")
-    print(n/2)
-    printes(n/2)
+num = int(input("Enter a number : "))
+d = len(str(num))
+result = 0
+temp = num
+while temp > 0:
+    digit = temp%10
+    digit = digit ** d
+    result = result + digit
+    temp = temp // 10
+    
 
-printes(10)
+if result == num:
+    print(f"{num} is an Armstrong number.")
+else:
+    print(f"{num} is not an Armstrong number.")
