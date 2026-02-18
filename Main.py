3de380cc9f31109097b73eb985e35d4a0603d051
goto-del <eliasmoncy27@gmail.com>
2026-02-18 15:25:10 +0530

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,11 +1,20 @@
-def swap(a,b):
-    print(f"Before Swapping a = {a}, b = {b} ")
-    a = a ^ b
-    b = a ^ b
-    a = a ^ b
-    print(f"After Swapping a = {a}, b = {b} ")
+def powerset(arr):
+    result = [[]]
+    for i in arr:
+        newset = []
+        for s in result:
+            newset.append(s+[i])
+        result = result+newset
+    return result
 
-a = int(input("Enter the value of a : "))
-b = int(input("Enter the value of b : "))
+n = int(input("How many numbers : "))
 
-swap(a,b)
+arr = []
+for i in range(n):
+    num = int(input("ENTER THE NUMBER WHICH YOU WANT TO ADD IN THE LIST : "))
+    arr.append(num)
+
+ps = powerset(arr)      
+print(f"The Powerset of the array is .")
+for i in ps:
+    print(i)
